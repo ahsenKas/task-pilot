3281cd70639999680dbc651d4091d52c2d6b8b1f
ahsenKas <kasahsen@gmail.com>
2026-08-26 16:34:55 +0300

    version-4

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const date = now.toLocaleDateString("en-US", {
 
 document.querySelector("#current-day").innerHTML = date;
 
-document.querySelector("#current-day").innerHTML = day;
+/* TIMER */
 
 const timerDisplay = document.querySelector("#timer-display");
 const timerButton = document.querySelector(".timer");
@@ -35,7 +35,9 @@ timerButton.addEventListener("click", () => {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
 
-    timerDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    timerDisplay.textContent = `${minutes}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
 
     if (totalSeconds <= 0) {
       clearInterval(timer);
@@ -45,4 +47,76 @@ timerButton.addEventListener("click", () => {
   }, 1000);
 
   timerButton.textContent = "Pause";
+});
+
+/* QUOTES */
+
+const quotes = {
+  0: {
+    image: "images/pazar.jpg",
+    quote: "Kendine inan ve olduğun kişi olmaktan asla vazgeçme.",
+    author: "Roy T. Bennett",
+  },
+
+  1: {
+    image: "images/pazartesi.jpg",
+    quote: "Başlamak için mükemmel olman gerekmez.",
+    author: "Zig Ziglar",
+  },
+
+  2: {
+    image: "images/sali.jpg",
+    quote: "Hayatta hiçbir şeyden korkmayın; sadece anlayın.",
+    author: "Marie Curie",
+  },
+
+  3: {
+    image: "images/carsamba.jpg",
+    quote: "Kendini bil.",
+    author: "Sokrates",
+  },
+
+  4: {
+    image: "images/persembe.jpg",
+    quote: "Başarı, her gün tekrarlanan küçük çabaların toplamıdır.",
+    author: "Robert Collier",
+  },
+
+  5: {
+    image: "images/cuma.jpg",
+    quote: "İster yapabileceğine inan, ister yapamayacağına; haklısın.",
+    author: "Henry Ford",
+  },
+
+  6: {
+    image: "images/cumartesi.jpg",
+    quote: "Başarı, cesaretini kaybetmeden ilerlemektir.",
+    author: "Winston Churchill",
+  },
+};
+const today = now.getDay();
+const todayQuote = quotes[today];
+
+document.querySelector(".quote").style.backgroundImage =
+  `url("${todayQuote.image}")`;
+
+document.querySelector("#quote-text").innerHTML = todayQuote.quote;
+
+document.querySelector("#quote-author").innerHTML = todayQuote.author;
+const icons = document.querySelectorAll(".time-icon");
+
+if (hour >= 6 && hour < 12) {
+  icons[0].style.display = "inline";
+} else if (hour >= 12 && hour < 18) {
+  icons[1].style.display = "inline";
+} else {
+  icons[2].style.display = "inline";
+}
+
+/*SIDEBAR-TOOGLE*/
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("closed");
 });
